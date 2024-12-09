417158550edcde107a4aff2772a8a4e2573b8927
sreejithtmurali <sreejithtmurali@gmail.com>
2024-12-09 11:05:29 +0530

    aadsa

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:mymain/app/app.locator.dart';
 import 'package:mymain/models/getallproduct/Data.dart';
 import 'package:mymain/models/getallproduct/GetAllProducts.dart';
 import 'package:mymain/services/user_service.dart';
@@ -15,69 +16,79 @@ import '../models/successresponsemodel.dart';
 
 class ApiService {
   final String _baseUrl = 'https://freeapi.luminartechnohub.com';
-  UserService userPreferences=UserService();
+ // UserService userPreferences=UserService();
 
-  Future<User?> login({required String email,required String password}) async {
-    final url=Uri.parse("$_baseUrl/login");
-    final headers={
-      'accept': 'application/json' ,
-      'Content-Type': 'application/json'
-    };
-    final body=jsonEncode({
-      "email": "$email",
-      "password": "$password"
-    });
-    try{
-      final reaponse= await http.post(url,headers:headers ,body: body);
-      if(reaponse.statusCode>=200 && reaponse.statusCode<=299){
-        var json =jsonEncode(reaponse.body);
-        var user=User.fromJson(json);
-        return user;
+   final userPreferences=locator<UserService>();
 
-      }
-
-    }catch(e){
-      throw Exception("something went wrong");
-    }
-    
-  }
-
-
-  // Future<User> login({
-  //   required String email,
-  //   required String password,
-  // }) async {
-  //   final url = Uri.parse('$_baseUrl/login');
-  //
-  //   final body = jsonEncode({
-  //     "email": email,
-  //     "password": password,
-  //   });
-  //   final headers = {
-  //     'accept': 'application/json',
-  //     'Content-Type': 'application/json',
+  // Future<User?> login({required String email,required String password}) async {
+  //   final url=Uri.parse("$_baseUrl/login");
+  //   final headers={
+  //     'accept': 'application/json' ,
+  //     'Content-Type': 'application/json'
   //   };
+  //   final body=jsonEncode({
+  //     "email": "$email",
+  //     "password": "$password"
+  //   });
+  //   print(body);
+  //   print(url);
+  //   try{
+  //     final reaponse= await http.post(url,headers:headers ,body: body);
+  //     if(reaponse.statusCode>=200 && reaponse.statusCode<=299){
+  //       var json =jsonEncode(reaponse.body);
+  //       var user=await User.fromJson(json);
+  //       if(user!=null) {
+  //         userPreferences.saveuser(user);
+  //         return user;
+  //       }
   //
-  //   try {
-  //     final response = await http.post(
-  //         url,
-  //         headers: headers,
-  //         body: body
-  //     );
   //
-  //     if (response.statusCode>=200 && response.statusCode<=299)
-  //     {
-  //       print(response.body);
-  //       return User.fromJson(jsonDecode(response.body));
   //     }
-  //     else
-  //     {
-  //       throw Exception('Failed to login. Please check your credentials.');
-  //     }
-  //   } catch (error) {
-  //     throw Exception('Failed to login: $error');
+  //
+  //   }catch(e){
+  //     throw Exception("something went wrong");
   //   }
+  //
   // }
+
+
+  Future<User> login({
+    required String email,
+    required String password,
+  }) async {
+    final url = Uri.parse('$_baseUrl/login');
+
+    final body = jsonEncode({
+      "email": email,
+      "password": password,
+    });
+    final headers = {
+      'accept': 'application/json',
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      final response = await http.post(
+          url,
+          headers: headers,
+          body: body
+      );
+
+      if (response.statusCode>=200 && response.statusCode<=299)
+      {
+        print(response.body);
+        User user= User.fromJson(jsonDecode(response.body));
+        userPreferences.saveuser(user);
+        return User.fromJson(jsonDecode(response.body));
+      }
+      else
+      {
+        throw Exception('Failed to login. Please check your credentials.');
+      }
+    } catch (error) {
+      throw Exception('Failed to login: $error');
+    }
+  }
 
 
 
